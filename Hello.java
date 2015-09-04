ead0d214bdcf2448401241e9b3d05675a5bb159b
jack <longqiang@terminus.io>
2015-09-04 17:47:12 +0800

    create a new file Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("hello world!!!") ;
+		for (int x = 0 ; x < 100; x ++) {
+			System.out.println(x) ;
+		}		
+	}
+
+}

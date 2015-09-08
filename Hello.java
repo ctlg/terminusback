415f8f0e3dd4eef2fff99a9552d8989b49e91476
Jack <longqiang521@126.com>
2015-09-08 21:40:19 +0800

    Modified Hello.java File

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("hello world!!!") ;
+		for (int x = 0 ; x < 100; x ++) {
+			System.out.println(x) ;
+		}	
+		System.out.println("hello world!!!") ;
+		for (int x = 0 ; x < 100; x ++) {
+			System.out.println(x) ;
+		}	
+	}
+}

150b30747d0673000b6c1cc677af842f6094dfca
Jack <longqiang521@126.com>
2015-09-08 22:33:45 +0800

    update attributes

--- Hello.java
+++ Hello.java
@@ -1,26 +1,7 @@
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("hello world!!!") ;
-		for (int x = 0 ; x < 100; x ++) {
-			System.out.println(x) ;
-		}	
-		System.out.println("hello world!!!") ;
-		for (int x = 0 ; x < 100; x ++) {
-			System.out.println(x) ;
-		}	
 		private Integer empno ;
 		private String ename ;
 		private String job ;
-		private Date hiredate ;
-		private Double sal ;
-		private Double comm ;
-		print() ;
-		fun() ;
-	}
-	public static void print() {
-			System.out.println("longqiang ai zao ting") ;
-	}
-	public static void fun() {
-		System.out.println("this is a demo!") ;
 	}
 }

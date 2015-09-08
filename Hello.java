36cbe1cadfa1cd1ad0bf0597eb0dcb03c7f74262
Jack <longqiang521@126.com>
2015-09-08 21:37:13 +0800

    update

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Demo {
 		System.out.println("hello world!!!") ;
 		for (int x = 0 ; x < 100; x ++) {
 			System.out.println(x) ;
-		}		
+		}	
+		System.out.println("hello world!!!") ;
 	}
-
 }
